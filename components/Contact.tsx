37329a875d5341f95c1ecffd3096094b608fccce
craigraphics <craigraphics@mail.com>
2024-10-25 13:04:44 -0700

    Refactor package.json and add @radix-ui/react-toast package

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,11 +2,14 @@
 
 import React, { useState, FormEvent, FC } from 'react';
 import { useMutation } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
+import { useToast } from '@/hooks/use-toast';
 
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Toaster } from '@/components/ui/toaster';
 
 interface EmailFormData {
   email: string;
@@ -38,6 +41,8 @@ const sendEmail = async (formData: EmailFormData): Promise<EmailResponse> => {
 const EmailForm: FC = () => {
   const [email, setEmail] = useState<string>('');
   const [message, setMessage] = useState<string>('');
+  const t = useTranslations('contact');
+  const { toast } = useToast();
 
   const mutation = useMutation({
     mutationFn: sendEmail,
@@ -49,13 +54,30 @@ const EmailForm: FC = () => {
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    mutation.mutate({ email, message });
+
+    try {
+      await mutation.mutateAsync({ email, message });
+
+      toast({
+        variant: 'success',
+        description: t('form.success'),
+        className: 'font-medium',
+      });
+    } catch (error) {
+      toast({
+        variant: 'error',
+        description: t('form.error'),
+        className: 'font-medium',
+      });
+    }
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 m-64">
+    <div className="max-w-md mx-auto p-4 flex flex-col content-center place-self-centerpy-14 mt-12 px-4 sm:px-6 lg:px-8">
+      <h2 className="text-3xl font-extrabold text-primary dark:text-primary-dark sm:text-3xl underline-heading mt-4">{t('title')}</h2>
+      <p className="my-8 font-medium ">{t('description')}</p>
       <form onSubmit={handleSubmit} className="space-y-4">
-        <Label htmlFor="email">Please add the email</Label>
+        <Label htmlFor="email">{t('form.email')} </Label>
         <Input
           type="email"
           name="email"
@@ -63,18 +85,18 @@ const EmailForm: FC = () => {
           required
           value={email}
           onChange={e => setEmail(e.target.value)}
-          placeholder="email@email.com"
+          placeholder={t('form.emailPlaceholder')}
           className="w-full p-2 border rounded !mt-1 !mb-4"
           disabled={mutation.isPending}
         />
-        <Label htmlFor="message">Please add the message</Label>
+        <Label htmlFor="message">{t('form.message')}</Label>
         <Textarea
           name="message"
           id="message"
           required
           value={message}
           onChange={e => setMessage(e.target.value)}
-          placeholder="Your message"
+          placeholder={t('form.messagePlaceholder')}
           className="w-full p-2 border rounded !mt-1"
           rows={4}
           disabled={mutation.isPending}
@@ -85,16 +107,10 @@ const EmailForm: FC = () => {
           disabled={mutation.isPending}
           className="w-full p-2 rounded bg-accent text-background dark:bg-accent-dark dark:text-background-dark hover:bg-secondary dark:hover:bg-secondary-dark disabled:bg-gray-400"
         >
-          {mutation.isPending ? 'Sending...' : 'Send'}
+          {mutation.isPending ? t('form.sending') : t('form.send')}
         </Button>
 
-        {mutation.isSuccess && <div className="mt-4 p-2 bg-green-100 text-green-700 rounded">Email sent successfully!</div>}
-
-        {mutation.isError && (
-          <div className="mt-4 p-2 bg-red-100 text-red-700 rounded">
-            {mutation.error instanceof Error ? mutation.error.message : 'Failed to send email. Please try again.'}
-          </div>
-        )}
+        <Toaster />
       </form>
     </div>
   );
